9821e9908b8a81ca5cb5d7ef187962346a77307a
Ikromov-fs <nodirikromov10@gmail.com>
2023-04-05 18:23:22 +0300

    new

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Register from './pages/register/register'
+import Login from './pages/login/login'
+import { Route, Routes } from 'react-router-dom'
+import Main from './pages/main/main'
+import './index.css'
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/main' element={<Main />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
